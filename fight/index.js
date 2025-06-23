04eb05cb4d85188b2ce77002a03d3ac72a7e3dfe
dlfjdfjdklf <ij784381@gmail.com>
2025-06-23 14:50:55 +0900

    first fight

--- fight/index.js
+++ fight/index.js
@@ -5,3 +5,28 @@ canvas.width = 1024;
 canvas.height = 578; 
 
 c.fillRect(0, 0, canvas.width, canvas.height);
+
+class Sprite {
+    constructor( { position, velocity } ) {
+        this.position = position;
+
+        this.width = 50;
+        this.height = 150;
+    }
+
+    draw() {
+        c.fillStyle = "red";
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+}
+
+// 플레이어(1P) 선
+const player = new Sprite({
+    position: {
+        x: 0,
+        y: 0,
+    }
+})
+
+console.log(player);
+player.draw();
